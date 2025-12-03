d802a5e021b62dcda1ab039b97bc6b09a1de7dc6
gdewaghe <guillaume.dewaghe@gmail.com>
2025-12-03 16:22:44 +0100

    MessengingDemo - Add ComputeFactorialMessage

---
+++ src/MessengingDemo/Messages/ComputeFactorialMessage.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace MessengingDemo.Messages
+{
+    public class ComputeFactorialMessage : RequestMessage<long>
+    {
+        public int BaseNumber { get; set; }
+    }
+}
