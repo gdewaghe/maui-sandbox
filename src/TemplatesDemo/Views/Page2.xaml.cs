d7f313ae5014b8aa2d9f6b44679d353c8b8543b8
gdewaghe <guillaume.dewaghe@gmail.com>
2025-12-03 21:13:34 +0100

    TemplatesDemo - Add Page2 and Page3 elements and add them to AppShell

---
+++ src/TemplatesDemo/Views/Page2.xaml.cs
@@ -0,0 +1,9 @@
+namespace TemplatesDemo.Views;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/TemplatesDemo/Views/Page3.xaml.cs
@@ -0,0 +1,9 @@
+namespace TemplatesDemo.Views;
+
+public partial class Page3 : ContentPage
+{
+	public Page3()
+	{
+		InitializeComponent();
+	}
+}
