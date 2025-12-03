5cdd8073bfbff0efda7bb6d321586c9b47465e26
gdewaghe <guillaume.dewaghe@gmail.com>
2025-12-03 15:29:23 +0100

    MessengingDemo - Configure MainPage and initialize DialogService

--- src/MessengingDemo/App.xaml.cs
+++ src/MessengingDemo/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace MessengingDemo
+﻿using MessengingDemo.Services;
+
+namespace MessengingDemo
 {
     public partial class App : Application
     {
+        public DialogService DialogService { get; }
+
         public App()
         {
             InitializeComponent();
+            DialogService = new();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
