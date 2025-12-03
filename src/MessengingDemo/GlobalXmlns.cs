a13a872d6f644e0995f8b48a5c1ad634072c2986
gdewaghe <guillaume.dewaghe@gmail.com>
2025-12-03 14:47:18 +0100

    Add MessengingDemo project

---
+++ src/MessengingDemo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MessengingDemo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MessengingDemo.Pages")]
