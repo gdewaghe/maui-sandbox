df61c1d3183dd55ed3530406e3448a98fba61743
gdewaghe <guillaume.dewaghe@gmail.com>
2025-12-03 15:27:55 +0100

    MessengingDemo - Add SimpleAlertMessage

---
+++ src/MessengingDemo/Messages/SimpleAlertMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace MessengingDemo.Messages
+{
+    public class SimpleAlertMessage
+    {
+        public required string Title { get; set; }
+        public required string Message { get; set; }
+        public required string Cancel { get; set; }
+    }
+}
